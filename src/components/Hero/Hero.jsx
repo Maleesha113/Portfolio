5b27e09f2f29c0243860f41f9ab636c1c26a07e7
Maleesha113 <w1953599@my.westminster.ac.uk>
2024-05-12 12:39:21 +0530

    create the about page

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -10,15 +10,15 @@ export const Hero = () => {
                 <h1 className={styles.title}> Hi, I'm Maleesha </h1>
                 <p className={styles.description}>
                 I am an ambitious undergraduate student pursuing a BSc (Hons) in Computer Science at the University of Westminster, Institute of Information Technology (IIT).
-                I am dedicated to expanding my knowledge and skills in the field of computer science. Eager to contribute to innovative projects and collaborate with industry professionals, 
-                I am committed to achieving academic and professional success.
+                {/* I am dedicated to expanding my knowledge and skills in the field of computer science. Eager to contribute to innovative projects and collaborate with industry professionals, 
+                I am committed to achieving academic and professional success. */}
                 </p>
                 <a href="mailto:myemail@email.com" className={styles.contactBtn}>
                     Contact Me
                 </a>
             </div>
             <img
-                src={getImageurl("hero/mypick1.png")}
+                src={getImageurl("hero/mypick2.png")}
                 alt="Hero image of me"
                 className={styles.heroImg}
             />
