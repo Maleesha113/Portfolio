3c2b1cc5e09d08de0a91bd5cd404ea14bc3fcc11
Maleesha113 <w1953599@my.westminster.ac.uk>
2024-05-14 21:16:10 +0530

    project card created

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,32 +1,19 @@
 import React from "react";
 
+import styles from "./Projects.module.css";
 import projects from "../../data/projects.json";
-import { getImageurl } from "../../utils";
+import { ProjectCard } from "./ProjectCard";
 
 export const Projects = () => {
     return(
-        <section>
-            <h2>Projects</h2>
-            <div>
+        <section className={styles.container}>
+            <h2 className={styles.title}>Projects</h2>
+            <div className={styles.projects}>
                 {
                     projects.map((project, id) => {
-                        return <div key={id}>
-                            <img src={getImageurl(project.imageSrc)} alt={`Image of ${project.title}`}></img>
-                            <h3>{project.title}</h3>
-                            <p>{project.description}</p>
-                            <ul>
-                                {
-                                    project.skills.map((skill, id) => {
-                                        <li key={id}>{skill}</li>
-                                    })
-                                }
-                            </ul>
-                            <div>
-                                <a href={project.demo}>Demo</a>
-                                <a href={project.source}>Source</a>
-                            </div>
-                        </div>
-                    })
+                        return(
+                        <ProjectCard key={id} project={project} />
+                    )})
                 }
             </div>
         </section>
